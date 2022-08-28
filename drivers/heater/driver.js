31329da2f129a44ca1d568f17b739a1306464a60
RonnyWinkler <ronny.winkler@web.de>
2022-08-28 21:52:51 +0200

    Adjusted heater driver
    Capabilities are added dynamically on pairing depending on device details.

--- drivers/heater/driver.js
+++ drivers/heater/driver.js
@@ -16,15 +16,57 @@ class TuyaHeaterDriver extends TuyaBaseDriver {
         else {
             let heater = this.get_devices_by_type("heater");
             for (let tuyaDevice of Object.values(heater)) {
-                let capabilities = ["onoff","target_temperature","measure_temperature","thermostat_heater_mode",
-                    ];
+                let capabilities = [];
+                let capabilitiesOptions = {};
                 this.log("Add heater, device details:");
                 this.log(tuyaDevice);
+                if (tuyaDevice.status){
+                    for (let i=0; i<tuyaDevice.status.length; i++){
+                        switch (tuyaDevice.status[i].code){
+                            case "temp_current":
+                                capabilities.push("measure_temperature");
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                if (tuyaDevice.functions){
+                    for (let i=0; i<tuyaDevice.functions.length; i++){
+                        let values;
+                        switch (tuyaDevice.functions[i].code){
+                            case "switch":
+                                capabilities.push("onoff");
+                                break;
+                            case "temp_set":
+                                values = JSON.parse(tuyaDevice.functions[i].values);
+                                capabilities.push("target_temperature");
+                                capabilitiesOptions["target_temperature"] = 
+                                    {
+                                        "min": values.min,
+                                        "max": values.max,
+                                        "step": values.step
+                                    };
+                                break;
+                            case "mode":
+                                values = JSON.parse(tuyaDevice.functions[i].values);
+                                if (values.range != null){
+                                    if (values.range.indexOf("auto") >= 0){
+                                        capabilities.push("thermostat_heater_mode");
+                                    }
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
                 devices.push({
                     data: {
                         id: tuyaDevice.id
                     },
                     capabilities: capabilities,
+                    capabilitiesOptions: capabilitiesOptions,
                     name: tuyaDevice.name
                 });
 
